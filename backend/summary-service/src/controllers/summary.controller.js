4d6f757302030711447c17353f7d6da990b27f2c
ironroggers <atul.bpq@gmail.com>
2025-10-25 20:11:49 +0530

    response updated

--- backend/summary-service/src/controllers/summary.controller.js
+++ backend/summary-service/src/controllers/summary.controller.js
@@ -3,7 +3,7 @@ import { getSummary } from "../services/summary.service.js";
 export async function getSummaryController(req, res, next) {
   try {
     const data = await getSummary();
-    res.json({ success: true, data });
+    res.json(data);
   } catch (err) {
     next(err);
   }
